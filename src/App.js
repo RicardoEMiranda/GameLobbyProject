e30e09df96d7a6b52b321baf415b0ff76bcb9ca6
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2022-11-11 21:08:24 -0500

    First commit.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className='HeaderStyle'>Game Lobby</h1>
+
+      <div className="appframe">
+        <div className="topleft">
+          <h2 className="consoleHeader">Player 1</h2>
+       
+        </div>
+
+        <div className="topright">
+          <h2 className="consoleHeader">Player 2</h2>
+        </div>
+
+        <div className="bottomleft">
+          <h2 className="consoleHeader">Player 3</h2>
+        </div>
+        
+        <div className="bottomright">
+          <h2 className="consoleHeader">Player 4</h2>
+        </div>
+
+      </div>
     </div>
+    
   );
 }
 
---
+++ src/LocaleContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const LocaleContext = React.CreateContent();
+
+
+export default LocaleContext;
